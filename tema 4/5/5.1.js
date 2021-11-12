b9ff9820e76ffa9f67372ae4c1adf52f169f4311
David Gonzalez <93193243+dagonzaaa@users.noreply.github.com>
2021-11-12 12:47:47 +0100

    Worksheet 5 tema 4

---
+++ tema 4/5/5.1.js
@@ -0,0 +1,39 @@
+var texto = "hola Me llamo @david"
+var mayuscula = /^[A-Z]+$/;
+var iChars = /[!@#$%^&]/;
+var correo = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+
+
+function validarMayuscula(txt) {
+	for (let i of txt){
+		if (mayuscula.test(i)) {
+			return true
+		}
+	}
+	return false
+}
+
+function validarCaracteresEspeciales(txt){
+	for (let i of txt){
+		if (iChars.test(i)) {
+			return true
+		}
+	}
+	return false
+}
+
+function validarCorreo(txt) {
+	return correo.test(txt)
+}
+
+function validarTarjetaCredito(txt) {
+	// body...
+}
+
+
+	
+
+
+document.write(validarMayuscula(texto)+"<br>")
+document.write(validarCaracteresEspeciales(texto)+"<br>")
+document.write(validarCorreo("dagonzalar@gmail.com"))
